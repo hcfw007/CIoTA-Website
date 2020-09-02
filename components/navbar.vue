1cf7eb94f2e26351118f3da836aac1446d1fe179
hcfw007 <brotherstyx@gmail.com>
2020-09-02 16:50:25 +0800

    base setup

---
+++ components/navbar.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="navbar">
+    <img src="@/static/images/logo.svg" alt="">
+  </div>
+</template>
+
+<style lang="stylus">
+.navbar
+  background-color: rgba(0, 21, 41, 1)
+  border: solid 1px rgba(50, 53, 95, 1)
+  box-shadow: 0px -5px 30px 0px rgba(37, 40, 75, 1) inset
+  height: 120px
+</style>

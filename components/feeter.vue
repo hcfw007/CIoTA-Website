3d2bcbf1faf487c622a05bcb62add718120daf8e
hcfw007 <brotherstyx@gmail.com>
2020-09-07 14:36:22 +0800

    feat: basic setup for footer

---
+++ components/feeter.vue
@@ -0,0 +1,37 @@
+<template>
+  <div class="footer-container">
+    <el-row>
+      <el-col :span="13" :offset="1">
+        <div class="menu-container">
+          <div class="menu-col">
+            <div class="menu-title">
+              联盟介绍
+            </div>
+            <div class="submenu-title">
+              联盟简介
+            </div>
+            <div class="submenu-title">
+              联盟成员
+            </div>
+            <div class="submenu-title">
+              宗旨理念
+            </div>
+          </div>
+        </div>
+      </el-col>
+    </el-row>
+  </div>
+</template>
+
+<style lang="stylus">
+.footer-container
+  color: #fff
+  background-color: rgb(37, 55, 72)
+
+  .menu-container
+    display: flex
+    flex-direction: column
+    justify-content: flex-start
+    text-align: center
+    width: 120px
+</style>

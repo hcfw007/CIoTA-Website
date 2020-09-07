8a9efccc1084ea9539240fd5a892248fd362dfdb
hcfw007 <brotherstyx@gmail.com>
2020-09-07 15:48:34 +0800

    feat: put menu structure into config file

---
+++ assets/website-config.js
@@ -0,0 +1,76 @@
+const menuStructure = {
+  name: 'root',
+  title: '首页',
+  addr: 'index',
+  children: [
+    {
+      name: 'intro',
+      title: '联盟介绍',
+      children: [
+        {
+          name: 'alliance-intro',
+          title: '联盟简介',
+          addr: 'alliance-intro'
+        }, {
+          name: 'alliance-member',
+          title: '联盟成员',
+          addr: 'alliance-member'
+        }, {
+          name: 'alliance-goal',
+          title: '宗旨理念',
+          addr: 'alliance-goal'
+        }
+      ]
+    }, {
+      name: 'conferences',
+      title: '物联大会',
+      children: [
+        {
+          name: 'past-conferences',
+          title: '往届回顾',
+          addr: 'past-conferences'
+        }
+      ]
+    }, {
+      name: 'info',
+      title: '资讯中心',
+      children: [
+        {
+          name: 'latest',
+          title: '最新资讯',
+          addr: 'latest'
+        }, {
+          name: 'trends',
+          title: '联盟动态',
+          addr: 'trends'
+        }, {
+          name: 'projects',
+          title: '相关项目',
+          addr: 'projects'
+        }
+      ]
+    }, {
+      name: 'members',
+      title: '会员中心',
+      children: [
+        {
+          name: 'member-list',
+          title: '会员展示',
+          addr: 'member-list'
+        }, {
+          name: 'how-to-apply',
+          title: '注册指引',
+          addr: 'how-to-apply'
+        }
+      ]
+    }, {
+      name: 'contact',
+      title: '联系我们',
+      addr: 'contact'
+    }
+  ]
+}
+
+export {
+  menuStructure
+}
--- components/feeter.vue
+++ components/feeter.vue
@@ -3,18 +3,23 @@
     <el-row>
       <el-col :span="13" :offset="1">
         <div class="menu-container">
-          <div class="menu-col">
+          <div v-for="menu in menuStructure" :key="menu.name" class="menu-col">
             <div class="menu-title">
-              联盟介绍
+              {{ menu.title }}
             </div>
-            <div class="submenu-title">
-              联盟简介
+            <div v-for="child in menu.children" :key="child.name" class="menu-subtitle">
+              {{ child.title }}
             </div>
-            <div class="submenu-title">
-              联盟成员
-            </div>
-            <div class="submenu-title">
-              宗旨理念
+            <div v-if="menu.name === 'contact'" class="contact">
+              <div class="address">
+                地址：
+              </div>
+              <div class="tel">
+                电话：
+              </div>
+              <div class="email">
+                邮箱：
+              </div>
             </div>
           </div>
         </div>
@@ -23,15 +28,55 @@
   </div>
 </template>
 
+<script>
+import { menuStructure } from '@/assets/website-config'
+
+export default {
+  data() {
+    return {
+      menuStructure: menuStructure.children
+    }
+  }
+}
+</script>
+
 <style lang="stylus">
 .footer-container
   color: #fff
   background-color: rgb(37, 55, 72)
 
   .menu-container
+    margin: 30px 0
     display: flex
-    flex-direction: column
+    flex-direction: row
     justify-content: flex-start
     text-align: center
-    width: 120px
+
+    .menu-title, .menu-subtitle
+      cursor: pointer
+
+    .menu-col
+      width: 120px
+
+      .menu-title
+        font-size: 16px
+        color: #F2F2F2
+
+      .menu-subtitle
+        margin-top: 30px
+        font-size: 12px
+        color: rgba(215, 215, 215, 0.749019607843137)
+
+        &:hover
+          color: rgb(24, 144, 255)
+
+    .contact
+      width: 120px
+
+      div
+        margin-top: 20px
+        font-size: 12px
+        color: rgba(215, 215, 215, 0.749019607843137)
+        text-align: left
+        padding-left: 30px
 </style>

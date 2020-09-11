c120035de97c7de22bf187cab2761f46af6bae5e
hcfw007 <brotherstyx@gmail.com>
2020-09-11 16:51:16 +0800

    feat: add trend-row

--- pages/index.vue
+++ pages/index.vue
@@ -16,7 +16,7 @@
     </el-row>
     <el-row class="block-row">
       <h3 class="block-title">联盟介绍</h3>
-      <el-col :span="12" class="intro-block-left content-block">
+      <el-col :span="12" class="content-block">
         <div class="block-logo">
           <img src="@/static/images/logo.png" alt="" class="intro-block-img">
           <img src="@/static/images/title.svg" alt="" class="intro-block-img">
@@ -24,7 +24,7 @@
         <p><span class="dark-blue">中国物联网产业技术创新战略联盟</span>中国物联网产业技术创新战略联盟由物联网部际联席会议的14部委推荐的40家单位发起，于2013年10月24日成立，中国电子科技集团公司任理事长单位。</p>
         <el-button type="primary">主要按钮</el-button>
       </el-col>
-      <el-col :span="12" class="intro-block-right content-block">
+      <el-col :span="12" class="content-block">
         <p>2013年10月24日，“物联网产业技术创新战略联盟”成立大会在京成功召开，科技部曹健林副部长出席会议并讲话，国家发展改革委、工信部等物联网发展部际联席会议14个部委作为联盟指导单位参会，来自40家联盟发起单位的代表参加了会议。</p>
         <p>联盟按照“战略引领、协同创新、产业推动、合作共赢”的原则组建，是由我国物联网产业链优势企业、科研院所、高校、组织共同发起组建的首个国家级物联网产业战略联盟。</p>
         <p>联盟将围绕产业政策研究、重大技术攻关、提供物联网公共服务等方面开展工作，并向全国公开征集联盟会员，形成面向行业的、开放的、具有国际先进水平的行业共性技术创新平台。</p>
@@ -32,6 +32,22 @@
     </el-row>
     <el-row class="block-row">
       <h3 class="block-title">联盟动态</h3>
+      <el-col :span="24" class="content-block">
+        <el-tabs tab-position="left">
+          <el-tab-pane>
+            <div slot="label" class="tab-title">物联网产业技术创新战略年会</div>
+            <img src="@/static/images/index/trend-1.jpg" alt="" class="trend-img">
+          </el-tab-pane>
+          <el-tab-pane>
+            <div slot="label" class="tab-title">第二界国际开放物联技术与标准峰会</div>
+            <img src="@/static/images/index/trend-2.png" alt="" class="trend-img">
+          </el-tab-pane>
+          <el-tab-pane>
+            <div slot="label" class="tab-title">第三界国际开放物联技术与标准峰会</div>
+            <img src="@/static/images/index/trend-3.png" alt="" class="trend-img">
+          </el-tab-pane>
+        </el-tabs>
+      </el-col>
     </el-row>
     <el-row class="block-row">
       <h3 class="block-title">物联大会</h3>
@@ -104,6 +120,54 @@ export default {
 
   .intro-block-img
     height: 40px
+
+  // 首页标签页的active样式
+  .el-tabs__nav.is-left
+    padding-right: 80px
+
+  .el-tabs__item.is-left
+    padding: 15px
+    height: auto
+
+  .tab-title
+    background-color: rgba(242, 242, 242, 0.447058823529412)
+    font-size: 30px
+    color: #333
+    text-align: center
+    padding: 20px
+    height: auto
+    border-radius: 15px
+    width: 100%
+
+  .el-tabs__nav-wrap.is-left::after
+    height: 0
+
+  .el-tabs__active-bar.is-left
+    height: 0 !important
+    width: 0
+    top: 35px
+    right: 10px
+    border-top: 21px solid transparent
+    border-bottom: 21px solid transparent
+    border-left: 30px solid rgb(24, 144, 255)
+    background-color: transparent
+
+    &::after
+      content: '.'
+      color: transparent
+      position: absolute
+      top: -15px
+      left: -33px
+      height: 0
+      width: 0
+      border-top: 15px solid transparent
+      border-bottom: 15px solid transparent
+      border-left: 24px solid #fff
+
+  .trend-img
+    height: 100%
+    max-height: 330px
+    width: 100%
 
 p
   text-indent: 2em

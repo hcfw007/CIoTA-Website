1a5373304dc75e894a2e8274a0bcefc0ee67f365
hcfw007 <brotherstyx@gmail.com>
2020-09-15 16:05:40 +0800

    feat: add alliance-member page

--- assets/getters.js
+++ assets/getters.js
@@ -70,3 +70,146 @@ export const memberGetter = function() {
     }
   ]
 }
+
+export const allMemberGetter = function() {
+  return {
+    tier1: {
+      title: '理事长',
+      list: [
+        {
+          index: 1,
+          name: '中国电子科技集团公司'
+        }
+      ]
+    },
+    tier2: {
+      title: '副理事长',
+      list: [
+        {
+          index: 2,
+          name: '中国航天科技集团公司'
+        }, {
+          index: 3,
+          name: '大唐电信科技产业集团'
+        }, {
+          index: 4,
+          name: '福建新大陆电脑股份有限公司'
+        }, {
+          index: 5,
+          name: '中国华录集团有限公司'
+        }, {
+          index: 6,
+          name: '上海物联网有限公司'
+        }, {
+          index: 7,
+          name: '广东物联天下科技集团股份有限公司'
+        }, {
+          index: 8,
+          name: '闪联信息技术工程中心有限公司'
+        }, {
+          index: 9,
+          name: '中国联合网络通信有限公司'
+        }, {
+          index: 10,
+          name: '中国电信集团公司'
+        }, {
+          index: 11,
+          name: '中国移动通信集团公司'
+        }, {
+          index: 12,
+          name: '工业和信息化部电信研究院通信标准研究所'
+        }, {
+          index: 13,
+          name: '总后后勤学院研究所'
+        }, {
+          index: 14,
+          name: '公安部第一研究所'
+        }, {
+          index: 15,
+          name: '公安部第三研究所'
+        }, {
+          index: 16,
+          name: '中国地质调查局水文地质环境地质调查中心'
+        }, {
+          index: 17,
+          name: '中国科学院物联网研究发展中心'
+        }, {
+          index: 18,
+          name: '工业和信息化部电子工业标准化研究院'
+        }, {
+          index: 19,
+          name: '中国物品编码中心'
+        }, {
+          index: 20,
+          name: '北京航空航天大学'
+        }, {
+          index: 21,
+          name: '东南大学科研院'
+        }, {
+          index: 22,
+          name: '南京邮电大学'
+        }, {
+          index: 23,
+          name: '解放军理工大学'
+        }, {
+          index: 24,
+          name: '中关村物联网产业联盟'
+        }, {
+          index: 25,
+          name: '中国智慧城市产业技术创新战略联盟'
+        }, {
+          index: 26,
+          name: '无锡“感知中国”物联网联盟'
+        }
+      ]
+    },
+    tier3: {
+      title: '理事',
+      list: [
+        {
+          index: 27,
+          name: '北京联众合为科技发展有限公司'
+        }, {
+          index: 28,
+          name: '北京时代凌宇科技有限公司'
+        }, {
+          index: 29,
+          name: '同方股份有限公司'
+        }, {
+          index: 30,
+          name: '中电海康集团有限公司'
+        }, {
+          index: 31,
+          name: '安徽四创电子股份有限公司'
+        }, {
+          index: 32,
+          name: '利尔达科技集团股份有限公司'
+        }, {
+          index: 33,
+          name: '方正国际软件有限公司'
+        }, {
+          index: 34,
+          name: '万达信息股份有限公司'
+        }, {
+          index: 35,
+          name: '武汉邮电科学研究院'
+        }, {
+          index: 36,
+          name: '公安部交通管理科学研究所'
+        }, {
+          index: 37,
+          name: '中国地质环境监测院地质灾害应急技术指导中心'
+        }, {
+          index: 38,
+          name: '中国国际电子商务中心'
+        }, {
+          index: 39,
+          name: '北京千方科技集团有限公司'
+        }, {
+          index: 40,
+          name: '清华大学'
+        }
+      ]
+    }
+  }
+}
---
+++ pages/intro/alliance-member.vue
@@ -0,0 +1,63 @@
+<template>
+  <div>
+    <el-table :data="members" class="member-table" :span-method="memberSpanMethod" border>
+      <el-table-column prop="index" label="序号" width="120" />
+      <el-table-column prop="title" label="联盟职务" width="180" />
+      <el-table-column prop="name" label="单位" />
+    </el-table>
+  </div>
+</template>
+
+<script>
+import { allMemberGetter } from '@/assets/getters'
+
+export default {
+  layout: 'article',
+  data() {
+    return {
+      members: []
+    }
+  },
+  created() {
+    let allMember = allMemberGetter()
+    let members = []
+    for (let i in allMember) {
+      let tier = allMember[i]
+      for (let item of tier.list) {
+        members.push({
+          index: item.index,
+          title: tier.title,
+          name: item.name
+        })
+      }
+    }
+    this.members = members
+  },
+  methods: {
+    memberSpanMethod({ columnIndex, row, rowIndex }) {
+      let members = this.members
+      if (columnIndex === 1) {
+        if (rowIndex > 1 && members[rowIndex - 1].title === row.title) {
+          return [0, 1]
+        } else {
+          let count = 1
+          for (let i = rowIndex + 1; i < members.length; i++) {
+            if (members[i].title === row.title) {
+              count += 1
+            } else {
+              break
+            }
+          }
+          return [count, 1]
+        }
+      }
+    }
+  }
+}
+</script>
+
+<style lang="stylus">
+.member-table
+  td, th
+    vertical-align: top
+</style>

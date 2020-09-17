9711a1d9acacd883db4744af862d972bea3f86b4
hcfw007 <brotherstyx@gmail.com>
2020-09-17 17:27:56 +0800

    feat: base setup for news detail page

---
+++ pages/info/latest/detail.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+    1
+  </div>
+</template>
+
+<script>
+export default {
+  layout: 'title-aside',
+  data() {
+    return {
+      news: {}
+    }
+  },
+  created() {
+    let newsId = this.$route.query.id
+    if (!newsId) {
+      this.$router.push('/info/latest')
+    }
+  }
+}
+</script>
+
+<style lang="stylus">
+@import '~assets/block-news-list.stylus'
+</style>

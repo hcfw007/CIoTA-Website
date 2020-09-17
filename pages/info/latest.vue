ef34f91d8e33e500c68d091b730bad0380e21fa3
hcfw007 <brotherstyx@gmail.com>
2020-09-17 16:53:38 +0800

    feat: add latest page

---
+++ pages/info/latest.vue
@@ -0,0 +1,32 @@
+<template>
+  <div>
+    <el-col :span="24" class="block-news-list">
+      <div v-for="(latest, index) in latestList" :key="'latest' + index" class="block-news">
+        <img :src="latest.imgSrc" alt="">
+        <div class="words-in-pic">{{ latest.title }}</div>
+        <div class="block-news-title">{{ latest.title }}</div>
+        <div class="block-news-content">{{ latest.content }}</div>
+      </div>
+    </el-col>
+  </div>
+</template>
+
+<script>
+import { latestGetter } from '@/assets/getters'
+
+export default {
+  layout: 'title-aside',
+  data() {
+    return {
+      latestList: []
+    }
+  },
+  created() {
+    this.latestList = latestGetter()
+  }
+}
+</script>
+
+<style lang="stylus">
+@import '~assets/block-news-list.stylus'
+</style>

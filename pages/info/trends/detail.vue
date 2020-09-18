f7ff803402e5ad456e249a7edec18b4845d1f0d4
hcfw007 <brotherstyx@gmail.com>
2020-09-18 15:26:56 +0800

    feat: add trends detail  and mock data

---
+++ pages/info/trends/detail.vue
@@ -0,0 +1,32 @@
+<template>
+  <div>
+    <el-col :span="24">
+      <h3>{{ trends.title }}</h3>
+      <div v-html="trends.content" />
+    </el-col>
+  </div>
+</template>
+
+<script>
+import { trendsDetailGetter } from '@/assets/getters'
+
+export default {
+  layout: 'title-aside',
+  data() {
+    return {
+      trends: {}
+    }
+  },
+  created() {
+    let trendsId = this.$route.query.id
+    if (!trendsId) {
+      this.$router.push('/info/trends')
+    } else {
+      this.trends = trendsDetailGetter(trendsId)
+    }
+  }
+}
+</script>
+
+<style lang="stylus">
+</style>

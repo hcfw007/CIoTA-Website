1d16ad516cd20dff95a64e43bb98e0031b4fb1d8
hcfw007 <brotherstyx@gmail.com>
2020-09-18 15:23:06 +0800

    feat: add trends page

---
+++ pages/info/trends/index.vue
@@ -0,0 +1,35 @@
+<template>
+  <div>
+    <el-col :span="24" class="row-news-list">
+      <div v-for="(trend, index) in trendsList" :key="'trend' + index" class="row-news" @click="goto(`/info/trends/detail?id=${ trend.id }`)">
+        <div class="row-news-title">{{ trend.title }}</div>
+        <div class="row-news-abstract">{{ trend.abstract }}</div>
+      </div>
+    </el-col>
+  </div>
+</template>
+
+<script>
+import { trendsGetter } from '@/assets/getters'
+
+export default {
+  layout: 'title-aside',
+  data() {
+    return {
+      trendsList: []
+    }
+  },
+  created() {
+    this.trendsList = trendsGetter()
+  },
+  methods: {
+    goto(url) {
+      this.$router.push(url)
+    }
+  }
+}
+</script>
+
+<style lang="stylus">
+@import '~assets/row-news-list.stylus'
+</style>
